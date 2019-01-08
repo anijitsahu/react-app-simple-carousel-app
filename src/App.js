cabebe380a1d3692c4001f0e687371fad58a60c2
AnijitSau <2anijit@gmail.com>
2019-01-08 12:15:39 +0530

    ErrorBoundary added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import React from 'react';
 // components 
 import Title from './components/Title';
 import Content from './components/Content';
+import ErrorBoundary from './components/ErrorBoundary'
 
 // css
 import './css/style.css'
 
 const App = () => {
   return (
-    <div className="container">
-    	{ /* including the Title as well as Content components */ }
-  		<Title />
-  		<Content />	
-  	</div>
+    <ErrorBoundary>
+	    <div className="container">
+	    	{ /* including the Title as well as Content components */ }
+		  		<Title />
+		  		<Content />	
+	  	</div>
+		</ErrorBoundary>
   );
 };
 
