438699c0fa3e8984a2046d1dd5159835a7861d79
anijit <anijitsau@gmail.com>
2021-03-21 15:25:08 +0530

    Extra code deleted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 // components 
 import Title from './components/Title';
 import Content from './components/Content';
-import ErrorBoundary from './components/ErrorBoundary'
 
 // css
 import './css/style.css'
 
 const App = () => {
 	return (
-		<ErrorBoundary>
-			<div className="container">
-				{ /* including the Title as well as Content components */}
-				<Title />
-				<Content />
-			</div>
-		</ErrorBoundary>
+		<div className="container">
+			{ /* including the Title as well as Content components */}
+			<Title />
+			<Content />
+		</div>
 	);
 };
 
